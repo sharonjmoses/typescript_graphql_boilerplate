6841085ae4fa9ce23503d398dcfe28779bdce6ea
sharonjmoses <smj.vit@gmail.com>
2021-03-28 22:33:56 +0530

    Added uuid

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,24 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
-
-@Entity()
-export class User {
-
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column()
-    firstName: string;
-
-    @Column()
-    lastName: string;
-
-    @Column()
-    age: number;
-
-}
+import {
+    Entity,
+    Column,
+    PrimaryColumn, 
+    BeforeInsert,
+    BaseEntity
+  } from "typeorm";
+  import {v4 as uuidv4} from 'uuid';
+  
+  @Entity()
+  export class User extends BaseEntity {
+    @PrimaryColumn("uuid") id: string;
+  
+    @Column("varchar", { length: 255 })
+    email: string;
+  
+    @Column("text") password: string;
+  
+    @BeforeInsert()
+    addId() {
+      this.id = uuidv4();
+    }
+  }
+  
