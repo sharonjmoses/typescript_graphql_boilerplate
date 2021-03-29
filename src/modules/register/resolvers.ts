d56da95f218f4771f45c45fce9e0438d08d7a5cc
sharonjmoses <smj.vit@gmail.com>
2021-03-29 07:00:01 +0530

    Added Schema Stitching

---
+++ src/modules/register/resolvers.ts
@@ -0,0 +1,23 @@
+
+import * as bcrypt from "bcryptjs";
+import { ResolverMap } from "../../types/graphql-utils";
+import { User } from "../../entity/User";
+
+export const resolvers: ResolverMap = {
+
+  Mutation: {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+
+      await user.save();
+      return true;
+    }
+  }
+};
